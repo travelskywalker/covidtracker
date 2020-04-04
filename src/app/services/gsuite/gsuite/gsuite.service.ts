133349781521b166c4f21e5d580bcea19e825f34
travelskywalker <blitzkrieg.nyler01@gmail.com>
2020-04-04 19:31:53 +0800

    spreadsheet data

--- src/app/services/gsuite/gsuite/gsuite.service.ts
+++ src/app/services/gsuite/gsuite/gsuite.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { resolve } from 'url';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,11 @@ export class GsuiteService {
   constructor(private http: HttpClient) { }
 
   getSpreadSheetData() {
-    var url = 'https://docs.google.com/spreadsheets/d/1G7J-Cjw8NWyAlKXEjUQsV5-5RdcRC8YBgJ7_ORLCAEQ/edit#gid=989351860';
+    var url = 'https://spreadsheets.google.com/feeds/cells/1G7J-Cjw8NWyAlKXEjUQsV5-5RdcRC8YBgJ7_ORLCAEQ/1/public/full?alt=json';
     this.http.get(url)
     .subscribe( data => {
-      console.log('data', data);
 
-
+      console.log(data)
     })
   }
 
@@ -24,3 +24,6 @@ export class GsuiteService {
     console.log('test')
   }
 }
+
+
+// https://spreadsheets.google.com/feeds/list/1G7J-Cjw8NWyAlKXEjUQsV5-5RdcRC8YBgJ7_ORLCAEQ/1/public/values?alt=json
