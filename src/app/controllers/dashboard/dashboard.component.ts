46e43415a880bdeffe72193d03c652e142530a50
travelskywalker <blitzkrieg.nyler01@gmail.com>
2020-04-09 18:33:48 +0800

    tableau integration

--- src/app/controllers/dashboard/dashboard.component.ts
+++ src/app/controllers/dashboard/dashboard.component.ts
@@ -20,8 +20,27 @@ export class DashboardComponent implements OnInit {
     public trackerService: TrackerService
   ) { }
 
-  async ngOnInit() {
+  ngOnInit() {
 
+    var divElement = document.getElementById('viz1586424330264');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    
+    var container = document.getElementById('container');
+
+    var containerHeight = container.offsetHeight;
+    var containerWidth = container.offsetWidth;
+
+    vizElement.style.height = containerHeight+'px';
+    vizElement.style.width = containerWidth+'px';
+
+    
+     var scriptElement = document.createElement('script');
+     scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+
+
+    return;
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
       'July', 'August', 'September', 'October', 'November', 'December'
     ];
